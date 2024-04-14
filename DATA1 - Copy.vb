55441d7a968048a9604a9363c977066b19c007fe
Duko-Deborah <dukodeborah@gmail.com>
2024-04-14 18:01:43 +0100

    new

---
+++ DATA1 - Copy.vb
@@ -0,0 +1,11 @@
+int main() {
+  Stack stack;
+  stack.push(10);
+  stack.push(20);
+  stack.push(30);
+  cout<< "Top element:"<< stack.top()<<< endl;
+  stack.pop();
+  cout<<"After pop, top element:"<< stack.top()<< endl;
+  cout<,:Stack size:"<< stack.size()<< endl;
+  retur O;
+}
